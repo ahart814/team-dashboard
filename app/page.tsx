4ee21eb9e4cba3dd27de0dcc0108c57eb12d4e60
Alex Hart <alexandra892011@gmail.com>
2025-07-09 15:20:40 -0400

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import TeamDashboardWithRealWeather from "../team-dashboard-real-data"
-import OpenWeatherAPITest from "../api-test"
+
 export default function Page() {
   return <TeamDashboardWithRealWeather />
-  // return <OpenWeatherAPITest />
 }
